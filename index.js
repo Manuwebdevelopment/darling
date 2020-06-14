40f46f9ba4b7b55817d6896a987dcab6125cee56
Manuwebdevelopment <61771922+Manuwebdevelopment@users.noreply.github.com>
2020-06-15 08:34:38 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+prompt("What is your name?");
+prompt("What's his name?");
+let loveScore = Math.random() * 100;
+loveScore = Math.floor (loveScore) + 1;
+if (loveScore >= 71) {
+alert("Your love score is: " + loveScore + "%. OMG! You'll love each other like Kanye loves Kanyes!");
+}
+if (loveScore >= 30 && loveScore <= 70); {
+alert("Your love score is: " + loveScore + "%. Not enough I reckon...lots of work");
+}
+if (loveScore <= 30) {
+     alert("Your love score is only: " + loveScore + "%. You go together like water and oil...Skip it");
+}
